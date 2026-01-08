5387726cdb936e96f70b251bf95ee759d51fe74c
NordboOyvind <nordbooyvind@gmail.com>
2026-01-08 13:40:45 +0100

    PostgreSQL version

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = "sett-en-sterk-nokkel-her"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///MiniGuardian.db"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///instance/MiniGuardian.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
