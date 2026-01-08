9dd41db5904bdb98806668e71057a1cfbbdd0682
NordboOyvind <nordbooyvind@gmail.com>
2026-01-08 13:42:14 +0100

    2nd

--- config.py
+++ config.py
@@ -2,7 +2,11 @@ import os
 
 class Config:
     SECRET_KEY = "sett-en-sterk-nokkel-her"
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///instance/MiniGuardian.db"
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = (
+        os.environ.get("DATABASE_URL")
+        or f"sqlite:///{os.path.join(basedir, 'instance', 'MiniGuardian.db')}"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
